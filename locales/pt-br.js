c0c8a0c90f398040f76f0fcff96dad581381c351
Vinicius Negrão <vnsn_@outlook.com>
2018-01-29 12:13:55 -0200

    added new locale for pt-br BRAZIL

---
+++ locales/pt-br.js
@@ -0,0 +1,6 @@
+var locale = {
+	'shortMonths':['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'],
+	'longMonths':['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+	'shortDays':['dom','seg','ter','qua','qui','sex','sáb'],
+	'longDays':['domingo','segunda-feira','terça-feira','quarta-feira','quinta-feira','sexta-feira','sábado']
+};
